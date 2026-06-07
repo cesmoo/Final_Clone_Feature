7a19f2cccadc36702a039d0fe79fe62b40a62894
my-sussues <ohmyqueenmedusa@gmail.com>
2026-06-07 08:33:25 +0630

    Update topup.py

--- topup.py
+++ topup.py
@@ -28,7 +28,7 @@ import database as db
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-OWNER_ID = int(os.getenv('OWNER_ID', 6437656033))
+OWNER_ID = int(os.getenv('OWNER_ID', 1318826936))
 GOOGLE_EMAIL = os.getenv('GOOGLE_EMAIL')
 GOOGLE_PASS = os.getenv('GOOGLE_PASS')
 
@@ -48,7 +48,7 @@ api_semaphore = asyncio.Semaphore(10)
 auth_lock = asyncio.Lock()
 
 # Clone Bot များ Code ဖြည့်လျှင် ဖြတ်မည့် Fees ရာခိုင်နှုန်း (ဥပမာ 0.05 = 5%)
-#CLONE_BOT_FEE_PERCENT = 0.05 
+CLONE_BOT_FEE_PERCENT = 0.05 
 
 BR_EMOJI = "5228878788867142213"   
 PH_EMOJI = "5231361434583049965"
